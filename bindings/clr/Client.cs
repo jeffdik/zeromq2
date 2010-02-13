664424b438ef0b653e55d34a7011f9682da71485
Jeff Dik <s450r1@gmail.com>
2010-02-13 15:05:17 -0500

    Added enough bindings to build Client.cs (a C# port of the client.cpp from the 0MQ documentation)

---
+++ bindings/clr/Client.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Zmq;
+
+public class Go
+{
+    public static int Main()
+    {
+        try {
+            Context ctx = new Context(1, 1, 0);
+            Socket s = new Socket(ctx, SocketType.REQ);
+            s.Connect("tcp://192.168.0.11:5555");
+
+            string query_string = "SELECT * FROM mytable";
+            Encoding encoding = Encoding.ASCII;
+            byte[] bytes = encoding.GetBytes(query_string);
+            Message query = new Message(bytes.Length+1);
+            Marshal.Copy(bytes, 0, query.Data, bytes.Length);
+            Marshal.WriteByte(query.Data, bytes.Length, 0);
+            s.Send(query, 0);
+
+            Message resultset = new Message();
+            s.Recv(resultset, 0);
+
+            string resultset_string = Marshal.PtrToStringAnsi(resultset.Data);
+            Console.WriteLine("Received response: '{0}'", resultset_string);
+        } catch (ZmqException e) {
+            Console.WriteLine("An error occurred: {0}\n", e.Message);
+            return 1;
+        }
+        return 0;
+    }
+}
--- bindings/clr/Zmq.cs
+++ bindings/clr/Zmq.cs
@@ -7,14 +7,14 @@ namespace Zmq
     {
         private int errno;
 
-        public ZmqException(int errno) : base(zmq_strerror(errno))
-        {
-            this.errno = errno;
-        }
-
         public int Errno
         {
             get { return errno; }
+        }
+
+        public ZmqException(int errno) : base(zmq_strerror(errno))
+        {
+            this.errno = errno;
         }
 
         [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
@@ -28,6 +28,11 @@ namespace Zmq
     public class Context
     {
         private IntPtr ptr;
+
+        public IntPtr Ptr
+        {
+            get { return ptr; }
+        }
 
         public Context(int app_threads, int io_threads, int flags)
         {
@@ -50,4 +55,136 @@ namespace Zmq
         static extern int zmq_term(IntPtr context);
     }
 
+    public enum SocketType
+    {
+        P2P,
+        PUB,
+        SUB,
+        REQ,
+        REP,
+        XREQ,
+        XREP,
+        UPSTREAM,
+        DOWNSTREAM
+    }
+
+    public class Socket
+    {
+        private IntPtr ptr;
+        private Context context;
+        private SocketType type;
+
+        public Socket(Context context, SocketType type)
+        {
+            this.context = context;
+            this.type = type;
+
+            ptr = zmq_socket(context.Ptr, type);
+            if (ptr == IntPtr.Zero)
+                throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        ~Socket()
+        {
+            int rc = zmq_close(ptr);
+            if (rc != 0)
+                throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        public void Connect(string addr)
+        {
+            int rc = zmq_connect(ptr, addr);
+            if (rc != 0)
+                throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        public bool Recv(Message msg, int flags)
+        {
+            int rc = zmq_recv(ptr, msg.Ptr, flags);
+            if (rc == 0)
+                return true;
+            if ((rc == -1) && ZmqException.zmq_get_errno() == 1)
+                return false;
+            throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        public bool Send(Message msg, int flags)
+        {
+            int rc = zmq_send(ptr, msg.Ptr, flags);
+            if (rc == 0)
+                return true;
+            if ((rc == -1) && (ZmqException.zmq_get_errno() == 1))
+                return false;
+            throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern int zmq_close(IntPtr context);
+
+        [DllImport("libzmq", CharSet = CharSet.Ansi,
+                   CallingConvention = CallingConvention.Cdecl)]
+        static extern int zmq_connect(IntPtr socket, string addr);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern int zmq_recv(IntPtr socket, IntPtr msg, int flags);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern int zmq_send(IntPtr socket, IntPtr msg, int flags);
+        
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr zmq_socket(IntPtr context, SocketType type);
+    }
+
+    public class Message
+    {
+        private IntPtr ptr;
+
+        public IntPtr Ptr
+        {
+            get { return ptr; }
+        }
+
+        // see bindings/c/zmq.h for calculating how big zmq_msg_t should be...
+
+        // #define ZMQ_MAX_VSM_SIZE 30
+
+        // typedef struct
+        // {
+        //     void *content;                              // 4 bytes
+        //     unsigned char shared;                       // 2 bytes
+        //     unsigned char vsm_size;                     // 2 bytes
+        //     unsigned char vsm_data [ZMQ_MAX_VSM_SIZE];  // 2*30 bytes
+        // } zmq_msg_t;
+        private const int ZMQ_MSG_T_SIZE = 68;
+
+        public Message()
+        {
+            ptr = Marshal.AllocHGlobal(ZMQ_MSG_T_SIZE);
+            int rc = zmq_msg_init(ptr);
+            if (rc != 0)
+                throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        public Message(int size)
+        {
+            ptr = Marshal.AllocHGlobal(ZMQ_MSG_T_SIZE);
+            int rc = zmq_msg_init_size(ptr, size);
+            if (rc != 0)
+                throw new ZmqException(ZmqException.zmq_get_errno());
+        }
+
+        public IntPtr Data
+        {
+            get { return zmq_msg_data(ptr); }
+        }
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern int zmq_msg_init(IntPtr msg);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern int zmq_msg_init_size(IntPtr msg, int size);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr zmq_msg_data(IntPtr msg);
+    }
 }
