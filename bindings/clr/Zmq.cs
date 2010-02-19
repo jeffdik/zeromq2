9b1929b9855e2014ba7d5f58c446b0cff9763c2b
Jeff Dik <s450r1@gmail.com>
2010-02-19 15:31:48 -0500

    refactor Zmq namespace: make Context, Socket, and Message implement the IDisposable pattern; internal C class to store native methods

--- bindings/clr/Zmq.cs
+++ bindings/clr/Zmq.cs
@@ -3,56 +3,76 @@ using System.Runtime.InteropServices;
 
 namespace Zmq
 {
-    public class ZmqException : Exception
+    internal class C
     {
-        private int errno;
+        // Context
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr zmq_init(int app_threads, int io_threads, int flags);
 
-        public int Errno
-        {
-            get { return errno; }
-        }
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_term(IntPtr context);
 
-        public ZmqException(int errno) : base(zmq_strerror(errno))
-        {
-            this.errno = errno;
-        }
+        // Socket
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_close(IntPtr socket);
 
+        [DllImport("libzmq", CharSet = CharSet.Ansi,
+                   CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_bind(IntPtr socket, string addr);
+
+        [DllImport("libzmq", CharSet = CharSet.Ansi,
+                   CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_connect(IntPtr socket, string addr);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_recv(IntPtr socket, IntPtr msg, int flags);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_send(IntPtr socket, IntPtr msg, int flags);
+        
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_setsockopt(IntPtr socket, SocketOption option, IntPtr optval, int optvallen); 
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr zmq_socket(IntPtr context, SocketType type);
+
+        // Message
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_msg_close(IntPtr msg);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_msg_init(IntPtr msg);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int zmq_msg_init_size(IntPtr msg, int size);
+
+        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr zmq_msg_data(IntPtr msg);
+
+        // Errors
         [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
         public static extern int zmq_get_errno();
 
         [DllImport("libzmq", CharSet = CharSet.Ansi,
                    CallingConvention = CallingConvention.Cdecl)]
-        static extern string zmq_strerror(int errnum);
+        public static extern string zmq_strerror(int errnum);
+
     }
 
-    public class Context
+    public enum SocketOption
     {
-        private IntPtr ptr;
-
-        public IntPtr Ptr
-        {
-            get { return ptr; }
-        }
-
-        public Context(int app_threads, int io_threads, int flags)
-        {
-            ptr = zmq_init(app_threads, io_threads, flags);
-            if (ptr == IntPtr.Zero)
-                throw new ZmqException(ZmqException.zmq_get_errno());
-        }
-
-        ~Context()
-        {
-            int rc = zmq_term(ptr);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
-        }
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern IntPtr zmq_init(int app_threads, int io_threads, int flags);
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_term(IntPtr context);
+        HWM=1,
+        LWM,
+        SWAP,
+        AFFINITY,
+        IDENTITY,
+        SUBSCRIBE,
+        UNSUBSCRIBE,
+        RATE,
+        RECOVERY_IVL,
+        MCAST_LOOP,
+        SNDBUF,
+        RCVBUF
     }
 
     public enum SocketType
@@ -68,111 +88,175 @@ namespace Zmq
         DOWNSTREAM
     }
 
-    public enum SocketOption
+    public class ZmqException : Exception
     {
-        HWM=1,
-        LWM,
-        SWAP,
-        AFFINITY,
-        IDENTITY,
-        SUBSCRIBE=6,
-        UNSUBSCRIBE,
-        RATE,
-        RECOVERY_IVL,
-        MCAST_LOOP,
-        SNDBUF,
-        RCVBUF
+        private int errno;
+
+        public int Errno
+        {
+            get { return errno; }
+        }
+
+        public ZmqException(int errno) : base(C.zmq_strerror(errno))
+        {
+            this.errno = errno;
+        }
+
+        public static void ThrowIfNotZero(int rc)
+        {
+            if (rc != 0)
+                Throw();
+        }
+
+        public static void ThrowIfNotZero(IntPtr ptr)
+        {
+            if (ptr == IntPtr.Zero)
+                Throw();
+        }
+
+        private static void Throw()
+        {
+                throw new ZmqException(C.zmq_get_errno());
+        }
     }
 
-    public class Socket
+    public class Context : IDisposable
     {
         private IntPtr ptr;
-        private Context context;
+
+        public Context(int app_threads, int io_threads, int flags)
+        {
+            ptr = C.zmq_init(app_threads, io_threads, flags);
+            ZmqException.ThrowIfNotZero(ptr);
+        }
+
+        ~Context()
+        {
+            Dispose(false);
+        }
+
+        public Socket CreateSocket(SocketType type)
+        {
+            IntPtr socket_ptr = C.zmq_socket(ptr, type);
+            ZmqException.ThrowIfNotZero(ptr);
+
+            return new Socket(socket_ptr, type);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // if (disposing)
+            // {
+            //     // Clean up all managed resources
+            // }
+            
+            if (ptr != IntPtr.Zero) {
+                int rc = C.zmq_term(ptr);
+                ptr = IntPtr.Zero;
+                ZmqException.ThrowIfNotZero(rc);
+            }
+        }
+            
+    }
+
+    public class Socket : IDisposable
+    {
+        private IntPtr ptr;
         private SocketType type;
 
-        public Socket(Context context, SocketType type)
-        {
-            this.context = context;
-            this.type = type;
+        // got EAGAIN by compiling and running the following C code with VC++ 2008 Express
+        // #include <errno.h>
+        // #include <stdio.h>
+        //
+        // int main()
+        // {
+        // 	printf("%i\n", EAGAIN);
+        // }
+        //
+        // E:\jad\repos\external\zeromq2\bindings\clr>..\..\builds\msvc\Release\whats_eagain.exe
+        // 11
+        private const int EAGAIN=11;
 
-            ptr = zmq_socket(context.Ptr, type);
-            if (ptr == IntPtr.Zero)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+        // Don't call this, call Context.CreateSocket
+        public Socket(IntPtr ptr, SocketType type)
+        {
+            this.ptr = ptr;
+            this.type = type;
         }
 
         ~Socket()
         {
-            int rc = zmq_close(ptr);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // if (disposing)
+            // {
+            //     // Clean up all managed resources
+            // }
+
+            if (ptr != IntPtr.Zero) {
+                int rc = C.zmq_close(ptr);
+                ptr = IntPtr.Zero;
+                ZmqException.ThrowIfNotZero(rc);
+            }
         }
 
         public void Bind(string addr)
         {
-            int rc = zmq_bind(ptr, addr);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+            int rc = C.zmq_bind(ptr, addr);
+            ZmqException.ThrowIfNotZero(rc);
         }
 
         public void Connect(string addr)
         {
-            int rc = zmq_connect(ptr, addr);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+            int rc = C.zmq_connect(ptr, addr);
+            ZmqException.ThrowIfNotZero(rc);
         }
 
         public bool Recv(Message msg, int flags)
         {
-            int rc = zmq_recv(ptr, msg.Ptr, flags);
-            if (rc == 0)
-                return true;
-            if ((rc == -1) && ZmqException.zmq_get_errno() == 1)
-                return false;
-            throw new ZmqException(ZmqException.zmq_get_errno());
+            int rc = C.zmq_recv(ptr, msg.Ptr, flags);
+            return CheckRecvSendReturnCode(rc);
         }
 
         public bool Send(Message msg, int flags)
         {
-            int rc = zmq_send(ptr, msg.Ptr, flags);
-            if (rc == 0)
-                return true;
-            if ((rc == -1) && (ZmqException.zmq_get_errno() == 1))
-                return false;
-            throw new ZmqException(ZmqException.zmq_get_errno());
+            int rc = C.zmq_send(ptr, msg.Ptr, flags);
+            return CheckRecvSendReturnCode(rc);
         }
 
         public int SetSockOpt(SocketOption option, IntPtr optval, int optvallen)
         {
-            int rc = zmq_setsockopt (ptr, option, optval, optvallen);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+            int rc = C.zmq_setsockopt (ptr, option, optval, optvallen);
+            ZmqException.ThrowIfNotZero(rc);            
             return rc;
         }
 
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_close(IntPtr context);
-
-        [DllImport("libzmq", CharSet = CharSet.Ansi,
-                   CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_bind(IntPtr socket, string addr);
-
-        [DllImport("libzmq", CharSet = CharSet.Ansi,
-                   CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_connect(IntPtr socket, string addr);
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_recv(IntPtr socket, IntPtr msg, int flags);
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_send(IntPtr socket, IntPtr msg, int flags);
-        
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_setsockopt(IntPtr socket, SocketOption option, IntPtr optval, int optvallen); 
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern IntPtr zmq_socket(IntPtr context, SocketType type);
+        private bool CheckRecvSendReturnCode(int rc)
+        {
+            if (rc == 0)
+                return true;
+            if ((rc == -1) && (C.zmq_get_errno() == EAGAIN))
+                return false;
+            throw new ZmqException(C.zmq_get_errno());
+        }
     }
 
-    public class Message
+    public class Message : IDisposable
     {
         private IntPtr ptr;
 
@@ -188,58 +272,69 @@ namespace Zmq
         // typedef struct
         // {
         //     void *content;                              // 4 bytes
-        //     unsigned char shared;                       // 2 bytes
-        //     unsigned char vsm_size;                     // 2 bytes
-        //     unsigned char vsm_data [ZMQ_MAX_VSM_SIZE];  // 2*30 bytes
+        //     unsigned char shared;                       // 1 bytes
+        //     unsigned char vsm_size;                     // 1 bytes
+        //     unsigned char vsm_data [ZMQ_MAX_VSM_SIZE];  // 1*30 bytes
         // } zmq_msg_t;
-        private const int ZMQ_MSG_T_SIZE = 68;
+
+        // better calculated by compiling and running the following C code
+        // #include <stdio.h>
+        // #include <zmq.h>
+        //
+        // void main()
+        // {
+        // 	printf("%i\n", sizeof(zmq_msg_t));
+        // }
+        //
+        // E:\jad\repos\external\zeromq2\bindings\clr>zmq_msg_t_size.exe
+        // 36
+        private const int ZMQ_MSG_T_SIZE = 36;
 
         public Message()
         {
             ptr = Marshal.AllocHGlobal(ZMQ_MSG_T_SIZE);
-            int rc = zmq_msg_init(ptr);
+            int rc = C.zmq_msg_init(ptr);
             if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+                throw new ZmqException(C.zmq_get_errno());
         }
 
         public Message(int size)
         {
             ptr = Marshal.AllocHGlobal(ZMQ_MSG_T_SIZE);
-            int rc = zmq_msg_init_size(ptr, size);
+            int rc = C.zmq_msg_init_size(ptr, size);
             if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+                throw new ZmqException(C.zmq_get_errno());
         }
 
         ~Message()
         {
-            int rc = zmq_msg_close(ptr);
-            Marshal.FreeHGlobal(ptr);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // if (disposing)
+            // {
+            //     // Clean up all managed resources
+            // }
+
+            if (ptr != IntPtr.Zero) {
+                int rc = C.zmq_msg_close(ptr);
+                Marshal.FreeHGlobal(ptr);
+                ptr = IntPtr.Zero;
+                ZmqException.ThrowIfNotZero(rc);
+            }
         }
 
         public IntPtr Data
         {
-            get { return zmq_msg_data(ptr); }
+            get { return C.zmq_msg_data(ptr); }
         }
-
-        public void Close()
-        {
-            int rc = zmq_msg_close(ptr);
-            if (rc != 0)
-                throw new ZmqException(ZmqException.zmq_get_errno());
-        }
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_msg_close(IntPtr msg);
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_msg_init(IntPtr msg);
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern int zmq_msg_init_size(IntPtr msg, int size);
-
-        [DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-        static extern IntPtr zmq_msg_data(IntPtr msg);
     }
 }
