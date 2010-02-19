c976073579ec7280823f8d154f8f0837856eeab4
Jeff Dik <s450r1@gmail.com>
2010-02-19 16:45:14 -0500

    fix C# examples Client.cs and Server.cs

--- bindings/clr/Client.cs
+++ bindings/clr/Client.cs
@@ -16,15 +16,14 @@ public class Go
             string query_string = "SELECT * FROM mytable";
             Encoding encoding = Encoding.ASCII;
             byte[] bytes = encoding.GetBytes(query_string);
-            Message query = new Message(bytes.Length+1);
+            Message query = new Message(bytes.Length);
             Marshal.Copy(bytes, 0, query.Data, bytes.Length);
-            Marshal.WriteByte(query.Data, bytes.Length, 0);
             s.Send(query, 0);
 
             Message resultset = new Message();
             s.Recv(resultset, 0);
 
-            string resultset_string = Marshal.PtrToStringAnsi(resultset.Data);
+            string resultset_string = Marshal.PtrToStringAnsi(resultset.Data, resultset.Size);
             Console.WriteLine("Received response: '{0}'", resultset_string);
         } catch (ZmqException e) {
             Console.WriteLine("An error occurred: {0}\n", e.Message);
--- bindings/clr/Server.cs
+++ bindings/clr/Server.cs
@@ -17,16 +17,15 @@ public class Go
         while (true) {
             using (Message query = new Message()) {
                 s.Recv(query, 0);
-                string query_string = Marshal.PtrToStringAnsi(query.Data);
+                string query_string = Marshal.PtrToStringAnsi(query.Data, query.Size);
                 Console.WriteLine("Received query: '{0}'", query_string);
             }
 
-            using (Message resultset = new Message(resultset_string.Length+1))
+            using (Message resultset = new Message(resultset_string.Length))
             {
                 Encoding encoding = Encoding.ASCII;
                 byte[] bytes = encoding.GetBytes(resultset_string);
                 Marshal.Copy(bytes, 0, resultset.Data, bytes.Length);
-                Marshal.WriteByte(resultset.Data, bytes.Length, 0);
                 s.Send(resultset, 0);
             }
         }
